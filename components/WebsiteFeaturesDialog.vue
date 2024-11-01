d9bc0149cf3e6dfcb993a69c39b45275b75fd3fe
dev-uros <uros.minic@kentkart.rs>
2024-11-01 12:56:33 +0100

    Changing title

--- components/WebsiteFeaturesDialog.vue
+++ components/WebsiteFeaturesDialog.vue
@@ -110,7 +110,7 @@ const logImages = [
       <template #header>
         <div class="flex items-center justify-between">
           <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
-            Backoffice application feature details
+            Website pages preview
           </h3>
           <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
                    @click="emit('closeDialog')"/>
