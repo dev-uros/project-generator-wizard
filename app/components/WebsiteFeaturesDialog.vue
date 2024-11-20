a318375be198e8f062ec0857109c34da1cf89ec8
dev-uros <uros.minic@kentkart.rs>
2024-11-20 23:54:02 +0100

    Setting iframe to new url

--- app/components/WebsiteFeaturesDialog.vue
+++ app/components/WebsiteFeaturesDialog.vue
@@ -74,7 +74,7 @@ const setSelectedStep = (index: number) => {
             <div v-if="item.label === 'Website'" class="w-full gap-4">
               <p class="mb-8">Website preview</p>
 
-              <iframe src="http://urosminic.com:3001/services" title="Website Template" class="w-full min-h-screen"></iframe>
+              <iframe src="https://nuxt-daisyui-website-preview.urosminic.com/" title="Website Template" class="w-full min-h-screen"></iframe>
             </div>
 
 
